fdcd6dad0a32de0d45e3e4f941f328fd57fd0983
Sanchit Garg <sanchit.garg@cloudsufi.com>
2025-04-21 13:59:06 +0530

    PLUGIN-1883: Add Named Table option to fetch the schema and PLUGIN-1896: Add Get Schema button to Named Table option

--- src/main/java/io/cdap/plugin/snowflake/source/batch/SnowflakeSourceAccessor.java
+++ src/main/java/io/cdap/plugin/snowflake/source/batch/SnowflakeSourceAccessor.java
@@ -17,10 +17,13 @@
 package io.cdap.plugin.snowflake.source.batch;
 
 import au.com.bytecode.opencsv.CSVReader;
+import com.google.common.base.Strings;
+import io.cdap.cdap.api.data.schema.Schema;
 import io.cdap.plugin.snowflake.common.SnowflakeErrorType;
 import io.cdap.plugin.snowflake.common.client.SnowflakeAccessor;
 import io.cdap.plugin.snowflake.common.util.DocumentUrlUtil;
 import io.cdap.plugin.snowflake.common.util.QueryUtil;
+import io.cdap.plugin.snowflake.common.util.SchemaHelper;
 import net.snowflake.client.jdbc.SnowflakeConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,6 +37,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * A class which accesses Snowflake API to do actions used by batch source.
@@ -77,7 +81,20 @@ public class SnowflakeSourceAccessor extends SnowflakeAccessor {
    */
   public List<String> prepareStageSplits() {
     LOG.info("Loading data into stage: '{}'", STAGE_PATH);
-    String copy = String.format(COMAND_COPY_INTO, QueryUtil.removeSemicolon(config.getImportQuery()));
+    String importQuery = config.getImportQuery();
+    if (Strings.isNullOrEmpty(importQuery)) {
+      String tableName = config.getTableName();
+      Schema schema = SchemaHelper.getParsedSchema(config.getSchema());
+      if (schema != null && schema.getFields() != null && !schema.getFields().isEmpty()) {
+        String columns = schema.getFields().stream()
+          .map(Schema.Field::getName)
+          .collect(Collectors.joining(","));
+        importQuery = String.format("SELECT %s FROM %s", columns, tableName);
+      } else {
+        importQuery = String.format("SELECT * FROM %s", tableName);
+      }
+    }
+    String copy = String.format(COMAND_COPY_INTO, QueryUtil.removeSemicolon(importQuery));
     if (config.getMaxSplitSize() > 0) {
       copy = copy + String.format(COMMAND_MAX_FILE_SIZE, config.getMaxSplitSize());
     }
@@ -94,10 +111,13 @@ public class SnowflakeSourceAccessor extends SnowflakeAccessor {
       }
     } catch (SQLException e) {
       String errorReason = String.format("Failed to load data into stage '%s' with sqlState %s and errorCode %s. " +
-          "For more details, see %s.", STAGE_PATH, e.getErrorCode(), e.getSQLState(),
-        DocumentUrlUtil.getSupportedDocumentUrl());
-      String errorMessage = String.format("Failed to load data into stage '%s' with sqlState %s and errorCode %s. " +
-        "Failed to execute query with message: %s.", STAGE_PATH, e.getSQLState(), e.getErrorCode(), e.getMessage());
+        "For more details, see %s.", STAGE_PATH, e.getErrorCode(), e.getSQLState(),
+          DocumentUrlUtil.getSupportedDocumentUrl());
+      String errorMessage = String.format(
+        "Failed to load data into stage '%s' with sqlState %s and errorCode %s. "
+          + "Failed to execute query with message: %s.",
+        STAGE_PATH, e.getSQLState(), e.getErrorCode(), e.getMessage()
+      );
       throw SnowflakeErrorType.fetchProgramFailureException(e, errorReason, errorMessage);
     }
     return stageSplits;
